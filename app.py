2fdf27e88386a49d723351ef93c647615c4f7989
joel tang <jztangw@gmail.com>
2025-02-11 22:45:36 -0800

    fix double call bug
    Signed-off-by: joel tang <jztangw@gmail.com>

--- app.py
+++ app.py
@@ -1,31 +1,21 @@
-# sqlite3 version bug https://github.com/chroma-core/chroma/issues/1985#issuecomment-2055963683
-# async pysqlite https://stackoverflow.com/a/71756224/4308025
-# __import__("aiosqlite")
-# import sys
-
-# sys.modules["sqlite3"] = sys.modules.pop("aiosqlite")
-
+import asyncio
 import os
 from pathlib import Path
 from typing import List
 
-import aiosqlite
 import chainlit as cl
+from chainlit import run_sync
 from dotenv import load_dotenv
 from langchain.callbacks.base import BaseCallbackHandler
-from langchain.indexes import SQLRecordManager, aindex, index
+from langchain.indexes import SQLRecordManager, aindex
 from langchain.prompts import ChatPromptTemplate
-from langchain.retrievers.multi_vector import MultiVectorRetriever
 from langchain.schema import Document, StrOutputParser
 from langchain.schema.runnable import Runnable, RunnableConfig, RunnablePassthrough
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores.chroma import Chroma
 from langchain_community.document_loaders import PyMuPDFLoader
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_postgres.vectorstores import PGVector
 from sqlalchemy.ext.asyncio import create_async_engine
-
-from utils.store import PostgresByteStore
 
 chunk_size = 1024
 chunk_overlap = 50
@@ -72,49 +62,20 @@ async def process_pdfs(pdf_storage_path: str, collection_name: str):
         record_manager,
         doc_search,
         cleanup="full",
-        # TODO: source id key likely wrong so that error finding index
         source_id_key="source",
     )
     print(f"Indexing stats: {index_result}")
 
-    # store = PostgresByteStore(os.getenv("POSTGRES_CONNECTION_STRING"), collection_name)
-    # id_key = "doc_id"
-
-    # retriever = MultiVectorRetriever(
-    #     vectorstore=doc_search,
-    #     docstore=store,
-    #     id_key=id_key,
-    # )
-
     return doc_search
-
-    # doc_search = Chroma.from_documents(docs, embeddings_model)
-
-    # namespace = "chromadb/my_documents"
-    # record_manager = SQLRecordManager(
-    #     namespace, db_url="sqlite:///record_manager_cache.sql"
-    # )
-    # record_manager.create_schema()
-
-    # index_result = index(
-    #     docs,
-    #     record_manager,
-    #     vectorstore,
-    #     cleanup="incremental",
-    #     source_id_key="source",
-    # )
-
-    # print(f"Indexing stats: {index_result}")
-
-    # return vectorstore
 
 
 model = ChatOpenAI(model_name="gpt-4o-mini", streaming=True)
 
+doc_search = asyncio.run(process_pdfs(PDF_STORAGE_PATH, "my_documents"))
+
 
 @cl.on_chat_start
 async def on_chat_start():
-    doc_search = await process_pdfs(PDF_STORAGE_PATH, "my_documents")
 
     template = """Answer the question based only on the following context:
 
@@ -144,13 +105,6 @@ async def on_message(message: cl.Message):
     runnable = cl.user_session.get("runnable")  # type: Runnable
     msg = cl.Message(content="")
 
-    # # TODO: how to handle doc upload
-    # print(len(message.elements))
-    # if message.elements:
-    #     await cl.Message(content=f"Received {message.elements[0]} image(s)").send()
-    #     await cl.Message(content=f"Received {len(message.elements)} doc(s)").send()
-    #     return
-
     class PostMessageHandler(BaseCallbackHandler):
         """
         Callback handler for handling the retriever and LLM processes.
@@ -176,8 +130,6 @@ async def on_message(message: cl.Message):
                     cl.Text(name="Sources", content=sources_text, display="inline")
                 )
 
-    # TODO: pgvector async stream available?
-    # async for chunk in runnable.astream(
     async for chunk in runnable.astream(
         message.content,
         config=RunnableConfig(
