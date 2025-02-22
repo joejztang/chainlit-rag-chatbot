366af127f5ae39878c8e941aa84d6d9fcf29614a
joel tang <jztangw@gmail.com>
2025-02-21 18:35:19 -0800

    fix process_pdfs logic bug
    Signed-off-by: joel tang <jztangw@gmail.com>

--- cl_app.py
+++ cl_app.py
@@ -6,37 +6,38 @@ from langchain.prompts import ChatPromptTemplate
 from langchain.schema import StrOutputParser
 from langchain.schema.runnable import Runnable, RunnableConfig, RunnablePassthrough
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
-from snowflake import SnowflakeGenerator
+from langchain_postgres.vectorstores import PGVector
 
+from utils.db import a_pgvector_engine
 from utils.handler import PostMessageHandler
 from utils.processor import process_pdfs
 from utils.util import save_file_to_disk
 
 load_dotenv()
-MACHINE_ID = os.getenv("MACHINE_ID", 1)
+USER_ID = os.getenv("USER_ID", "test")
 PDF_STORAGE_PATH = "./pdfs"
 
-sfgen = SnowflakeGenerator(MACHINE_ID)
 embeddings_model = OpenAIEmbeddings()
 model = ChatOpenAI(model_name="gpt-4o-mini", streaming=True)
-# doc_search = asyncio.run(
-#     process_pdfs(PDF_STORAGE_PATH, "my_documents", embeddings_model)
-# )
 
 
 @cl.on_chat_start
 async def on_chat_start():
     """Set the prompt for the user."""
-    template = """Answer the question based only on the following context:
+    template = """Quesition: {question}"""
+    prompt = ChatPromptTemplate.from_template(template)
+    cl.user_session.set("prompt", prompt)
+
+    file_template = """Answer the question based only on the following context:
 
     {context}
 
     Question: {question}
     """
-    prompt = ChatPromptTemplate.from_template(template)
-    sid = next(sfgen)
-    cl.user_session.set("prompt", prompt)
-    cl.user_session.set("sid", sid)
+    file_prompt = ChatPromptTemplate.from_template(file_template)
+    cl.user_session.set("file_prompt", file_prompt)
+
+    cl.user_session.set("uid", USER_ID)
 
 
 @cl.on_message
@@ -50,40 +51,57 @@ async def on_message(message: cl.Message):
     def format_docs(docs):
         return "\n\n".join([d.page_content for d in docs])
 
-    prompt = cl.user_session.get("prompt")  # type: Runnable
+    file_prompt = cl.user_session.get("file_prompt")  # type: Runnable
+    prompt = cl.user_session.get("prompt")
+    uid = cl.user_session.get("uid")
 
     msg = cl.Message(content="")
 
+    runnable = {"question": RunnablePassthrough()} | prompt | model | StrOutputParser()
     if message.elements:
         print(message.elements)
+
+        collections = set()
         for element in message.elements:
             await cl.make_async(save_file_to_disk)(
                 element.path, os.path.join(PDF_STORAGE_PATH, element.name)
             )
-        doc_search = await process_pdfs(
-            PDF_STORAGE_PATH, "my_documents", embeddings_model
+            collections.add(os.path.join(uid, element.name))
+        cl.user_session.set("collections", collections)
+
+        doc_search = PGVector(
+            embeddings_model,
+            collection_name=f"{uid}/{cl.user_session.get("id")}",
+            connection=a_pgvector_engine,
+            use_jsonb=True,
         )
+
+        await process_pdfs(
+            uid,
+            message.elements,
+            doc_search,
+        )
+
         retriever = doc_search.as_retriever()
         runnable = (
             {"context": retriever | format_docs, "question": RunnablePassthrough()}
-            | prompt
+            | file_prompt
             | model
             | StrOutputParser()
         )
 
-        async for chunk in runnable.astream(
-            message.content,
-            config=RunnableConfig(
-                callbacks=[cl.LangchainCallbackHandler(), PostMessageHandler(msg)]
-            ),
-        ):
-            await msg.stream_token(chunk)
+    async for chunk in runnable.astream(
+        message.content,
+        config=RunnableConfig(
+            callbacks=[cl.LangchainCallbackHandler(), PostMessageHandler(msg)]
+        ),
+    ):
+        await msg.stream_token(chunk)
 
-        await msg.send()
-    else:
-        await cl.Message("no file uploaded").send()
+    await msg.send()
 
 
 @cl.on_chat_end
-def on_chat_end():
-    print("The user disconnected!")
+async def on_chat_end():
+    """Delete the index and vectors."""
+    print("Done deleting index and vectors.")
--- utils/processor.py
+++ utils/processor.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from typing import Any, List
 
@@ -14,41 +15,46 @@ chunk_overlap = 50
 
 
 async def process_pdfs(
-    pdf_storage_path: str,
-    collection_name: str,
-    embeddings_model: Any,
+    uid: str,
+    pdfs: List[Any],
+    vectordb: Any,
 ) -> PGVector:
-    pdf_directory = Path(pdf_storage_path)
+    # pdf_directory = Path(pdf_storage_path)
     docs = []  # type: List[Document]
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=chunk_size, chunk_overlap=chunk_overlap
     )
 
-    for pdf_path in pdf_directory.glob("*.pdf"):
-        loader = PyMuPDFLoader(str(pdf_path))
+    for element in pdfs:
+        loader = PyMuPDFLoader(element.path)
         documents = loader.load()
         docs += text_splitter.split_documents(documents)
         # "source" will be used as group_id in upsertion_record table
         for doc in docs:
-            doc.metadata["source"] = sid
+            doc.metadata["source"] = os.path.join(uid, element.name)
 
-    doc_search = PGVector(
-        embeddings_model,
-        collection_name=collection_name,
-        connection=a_pgvector_engine,
-        use_jsonb=True,
-    )
+    # for pdf_path in pdf_directory.glob("*.pdf"):
+    #     loader = PyMuPDFLoader(str(pdf_path))
+    #     documents = loader.load()
+    #     docs += text_splitter.split_documents(documents)
+    #     # "source" will be used as group_id in upsertion_record table
+    #     for doc in docs:
+    #         doc.metadata["source"] = os.path.join(uid, pdf_path.name)
 
-    namespace = "pgvector/my_documents"
-    record_manager = LocalRecordManager(namespace)
+    # doc_search = PGVector(
+    #     embeddings_model,
+    #     collection_name=collection_name,
+    #     connection=a_pgvector_engine,
+    #     use_jsonb=True,
+    # )
+
+    record_manager = LocalRecordManager(uid)
 
     index_result = await aindex(
         docs,
         record_manager,
-        doc_search,
+        vectordb,
         cleanup="incremental",
         source_id_key="source",
     )
     print(f"Indexing stats: {index_result}")
-
-    return doc_search
